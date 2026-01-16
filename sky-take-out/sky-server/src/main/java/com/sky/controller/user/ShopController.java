40a0ee2a67111105d83b229e032aaeb6666c9b39
candong <2232802210@qq.com>
2026-01-16 21:47:48 +0800

    苍穹外卖——后端工程：店铺营业状态业务功能开发

---
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,32 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 店铺管理
+ * @author can dong
+ */
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+public class ShopController {
+    private static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 获取营业状态
+     * @return 营业状态
+     */
+    @GetMapping("/status")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取店铺营业状态：{}", status == 1 ? "营业中" : "打烊中");
+        return Result.success(status);
+    }
+}
